47e19954e1584b2bd64207c380ad3c40eede6f12
Paul Maskelyne <mu77ley@gmail.com>
2023-06-05 12:56:09 +0100

    custom logging interface

---
+++ system/src/utils/Logger.mjs
@@ -0,0 +1,25 @@
+import { SYSTEM_ID, SYSTEM_NAME } from "../constants.mjs";
+
+export default class Logger {
+	static DEBUG_ENABLED = null;
+
+	static debug(...args) {
+		if (this.DEBUG_ENABLED === null) {
+			this.DEBUG_ENABLED = game.settings.get(SYSTEM_ID, "debugEnabled");
+		}
+
+		if (this.DEBUG_ENABLED) console.debug(`${SYSTEM_NAME} |`, ...args);
+	}
+
+	static error(...args) {
+		console.error(`${SYSTEM_NAME} |`, ...args);
+	}
+
+	static log(...args) {
+		console.log(`${SYSTEM_NAME} |`, ...args);
+	}
+
+	static warn(...args) {
+		console.warn(`${SYSTEM_NAME} |`, ...args);
+	}
+}
